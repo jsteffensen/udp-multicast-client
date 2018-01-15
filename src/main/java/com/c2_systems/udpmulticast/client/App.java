454cff17ab6738c1c1ca94f9a5d81172d3f4e90f
Jesper Steen Steffensen <00121913@VFKINET.DK>
2018-01-15 16:11:31 +0100

    add netty to pom

--- src/main/java/com/c2_systems/udpmulticast/client/App.java
+++ src/main/java/com/c2_systems/udpmulticast/client/App.java
@@ -1,11 +1,6 @@
 package com.c2_systems.udpmulticast.client;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ src/main/java/com/c2_systems/udpmulticast/client/UDPClient.java
@@ -0,0 +1,14 @@
+package com.c2_systems.udpmulticast.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.ChannelOption;
+import io.netty.util.NetUtil;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.List;
+
+public class UDPClient {
+
+}
